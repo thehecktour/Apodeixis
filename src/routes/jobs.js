ace0b632807834a4feeb16c27e2c6f7d1bca5f51
Atilio <thehacktour@gmail.com>
2025-05-25 16:33:39 +0100

    refactor: 🐳 delete everything and start again

--- src/routes/jobs.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getJobs } = require('../controllers/jobController');
-
-const router = express.Router();
-
-router.get('/jobs', getJobs);
-
-module.exports = router;
